a3a3e8f13c9ceec0ed2a540532b546a38b925d73
Goran Mržljak <goran.mrzljak@gmail.com>
2021-07-20 10:42:27 +0200

    chore(project): some project structure updates
    Put tsconfig.eslint.json into every project.
    
    In 'file-system' move manual-tests and test-data to separate test folder.

--- packages/file-system/test/manual-tests/copy-folder.ts
+++ packages/file-system/test/manual-tests/copy-folder.ts
@@ -1,8 +1,8 @@
-import { resolvePath, resolvePathFromCwd } from '../../path';
-import { copyDirectorySubset } from '../../file-system';
+import { resolvePath, resolvePathFromCwd } from '../../src/path';
+import { copyDirectorySubset } from '../../src/file-system';
 
 async function test(): Promise<void> {
-  const testDataDir = resolvePathFromCwd('test-data');
+  const testDataDir = resolvePathFromCwd('test/test-data');
 
   const inputDir = resolvePath(testDataDir, 'input-dir');
   const outputDir = resolvePath(testDataDir, 'output-dir');
