1841bd9ca3cd3f003532153a7f79137afeb63bdf
Goran Mržljak <goran.mrzljak@gmail.com>
2021-08-04 00:21:53 +0200

    feat(browser-utils): add cookie/local/session storage apis

---
+++ packages/browser-utils/src/index.ts
@@ -0,0 +1,7 @@
+export { CookieWrapper } from './types/cookie-wrapper';
+export { StorageWrapper } from './types/storage-wrapper';
+
+export { createCookieWrapper } from './storage/cookie-wrapper';
+export { createStorageWrapper } from './storage/storage-wrapper';
+export { createLocalStorageWrapper } from './storage/local-storage';
+export { createSessionStorageWrapper } from './storage/session-storage';
