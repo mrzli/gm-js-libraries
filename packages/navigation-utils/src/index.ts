c935d2b03ca27f3c98f32b06eb321f14ddd862fc
Goran Mržljak <goran.mrzljak@gmail.com>
2021-08-08 12:08:29 +0200

    feat(navigation-utils): add history api export to index

--- packages/navigation-utils/src/index.ts
+++ packages/navigation-utils/src/index.ts
@@ -1,3 +1,5 @@
 export { LocationWrapper } from './types/location-wrapper';
+export { HistoryWrapper } from './types/history-wrapper';
 
 export { createLocationWrapper } from './navigation/location-wrapper';
+export { createHistoryWrapper } from './navigation/history-wrapper';
